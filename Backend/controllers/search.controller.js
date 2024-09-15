0e0dd5efffcfe649695c6cddd9e9912132faccb7
Manish6523 <220101005092@gandhinagaruni.ac.in>
2024-09-15 23:23:23 +0530

    TMDBproblem in TV controller and search

--- Backend/controllers/search.controller.js
+++ Backend/controllers/search.controller.js
@@ -1,5 +1,5 @@
 import User from "../models/user.model.js";
-import { fetchFromTMDB } from "../services/TMDB.services.js"
+import { fetchFromTMDB } from "../services/tmdb.services.js"
 
 export async function searchPerson(req, res) {
     const {query} = req.params
