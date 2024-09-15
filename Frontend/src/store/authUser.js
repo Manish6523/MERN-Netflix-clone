2bd679129a1f3de856b1387a3ea591844e131ebb
Manish6523 <133084768+Manish6523@users.noreply.github.com>
2024-09-16 00:07:38 +0530

    Update authUser.js

--- Frontend/src/store/authUser.js
+++ Frontend/src/store/authUser.js
@@ -24,7 +24,7 @@ export const useAuthStore = create((set) =>
     login: async (credentials) => {
         set({ isLoggingIn: true });
         try {
-            const response = await axios.post(`http://localhost:5000/api/v1/auth/login`,credentials,{Headers:"Access-Control-Allow-Origin: *"}, {withCredentials:true})
+            const response = await axios.post(`http://localhost:5000/api/v1/auth/login`,credentials,{Headers:"Access-Control-Allow-Origin: http://localhost:5173"}, {withCredentials:true})
             set( { user: response.data.user, isLoggingIn: false });
             toast.success("Logged in successfully");
         } catch (error) {
